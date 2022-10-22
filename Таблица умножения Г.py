3010a446c2ab8ee2b237bd1bca4e8423e71ab711
kirusha02301 <114716562+kirusha02301@users.noreply.github.com>
2022-10-22 08:26:57 +0700

    Delete Таблица умножения Г.py

--- Таблица умножения Г.py
+++
@@ -1,7 +0,0 @@
-p = int(input('Введите p (2<p<=10): '))
-print (f'{p}-ичная таблица умножения')
-for x in range (1,p):
-    for y in range (1,p):
-        z = (x*y // p)*10+(x*y)%p
-        print (z, end = ' ')
-    print ()
