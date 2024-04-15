9624cae9f6bef127f3f0efe2f0611485853bcc44
kirusha02301 <114716562+kirusha02301@users.noreply.github.com>
2024-04-15 18:53:00 +0700

    Add files via upload

---
+++ угадай число.py
@@ -0,0 +1,10 @@
+import random
+a=int(input('Введите диапозон челых чисел от: '))
+b=int(input('до: '))
+c = random.randint (a,b)
+i = -1
+i=int(input("Введите число"))
+while i != c:
+    print("неверно")
+    i=int(input("Введите число"))
+print ('Вы выграли')
