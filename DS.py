96690b2c850880a377e394b3a4dc3ef91db5fb24
kirusha02301 <114716562+kirusha02301@users.noreply.github.com>
2024-04-15 18:59:14 +0700

    Update DS.py

--- DS.py
+++ DS.py
@@ -1,47 +1,58 @@
-import discord
+def prov(A):
+    for x in range(1,100000):
+        F = ((A+x<123)<=((x%5==0)<=(not(x%7==0))))
+        if F==False:return False
+    return True
+for A in range(1,1000):
+    if prov(A):
+        print(A)
+        break
 
-client = discord.Client()
+f= open('17-378.txt')
+b=[]
+maxabs=-10000
+kol=0
+for i in f:
+    b.append(int(i))
+    if abs(int(i))%1001==0:
+        maxabs=max(maxabs,abs(int(i)))
+mins=11111111
+for i in range(len(b)-1):
+    a1,a2=b[i],b[i+1]
+    if len(str(abs(a1)))==3 or len(str(abs(a2)))==3:
+        if a1+a2>maxabs:
+            kol+=1
+            mins=min(mins,(a1+a2))
+print(kol,mins)
+
+# номера 6 7 10 11 12 16 18 22
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith("да"):
-        await message.channel.send('Пизда')
-    elif message.content.startswith('Да'):
-        await message.channel.send('Пизда')
-    elif message.content.startswith('Нет'):
-        await message.channel.send('Еблет')
-    elif message.content.startswith('А'):
-        await message.channel.send('Пошел нахуй')
-    elif message.content.startswith('а'):
-        await message.channel.send('Пошел нахуй')
-    elif message.content.startswith('Че'):
-        await message.channel.send('хуй через плечо не горячо если горячо переложи на другое плечо')
-    elif message.content.startswith('спасибо'):
-        await message.channel.send('за спасибо ебут красиво')
-    elif message.content.startswith('Спасибо'):
-        await message.channel.send('за спасибо ебут красиво')
-    elif message.content.startswith('нахуй'):
-        await message.channel.send('Сам иди')
-    elif message.content.startswith('кто'):
-        await message.channel.send('хуйло')
-    elif message.content.startswith('заеб'):
-        await message.channel.send('закрой свой рот')
-    elif message.content.startswith('кв'):
-        await message.channel.send('отьебись со своим кв')
-    elif message.content.startswith('@everyone'):
-        await message.channel.send('Заебал, не пингуй, сука')
-    elif message.content.startswith('Еблан'):
-        await message.channel.send('Звали пидоры?')
-    elif message.content.startswith('eблан'):
-        await message.channel.send('Звали пидоры?')
-    elif message.content.startswith('Ладно'):
-        await message.channel.send('Хую прохладно')
-    elif message.content.startswith('ладно'):
-        await message.channel.send('Хую прохладно')
+def game(x,y,h):
+    if (x<10 or y<10) and (h==4 or h==2):return True
+    elif x>=10 and y>=10 and h<4:
+        a = [game(x-3,y,h+1),game(x-1,y,h+1),game(x,y-1,h+1),game(x,y-3,h+1)]
+        if h==0 or h==2:return all(a)
+        if h==1 or h==3:return any(a)
+    return False
+for s in range(10,100):
+    if game(13,s,0):
+        print(s)
 
-client.run(
-    'MTAwMzk3NTI3NTYyNjkwOTc1Nw.Godmoa.jRXvPeCgG6-DCoy_O8G6OpNaGakT-XkihRvK4A')
+def f(x,stop):
+    if x==stop:return True
+    if x<stop:return False
+    else:
+        return f(x//2,stop) + f(x-2,stop)
+print(f(28,10)*f(10,1))
+
+
+f=open('24-111.txt').readline()
+
+S =f.replace('A','1').replace('B','1').replace('X','1')
+b = S.split('1')
+maxs=0
+for i in range(len(b)-6):
+    if len(b[i]+b[i+1]+b[i+2]+b[i+3]+b[i+4]+b[i+5])>maxs:maxs=len(b[i]+b[i+1]+b[i+2]+b[i+3]+b[i+4]+b[i+5])
+print(maxs+5)
