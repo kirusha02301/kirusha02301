2b38d5915b496bc3193805d62e498a7aefe54868
kirusha02301 <114716562+kirusha02301@users.noreply.github.com>
2024-04-15 18:49:21 +0700

    Update x.py

--- turtle/x.py
+++ turtle/x.py
@@ -1,36 +1,63 @@
-from turtle import*
-from math import*
-def fractal(aturt, depth, maxdepth):
-    if depth > maxdepth:
-        return
-    length = 180*((sqrt(2)/2)**depth)
-    anotherturt = aturt.clone()
-    aturt.forward(length)
-    aturt.left(45)
-    fractal(aturt, depth+1, maxdepth)
-    anotherturt.right(90)
-    anotherturt.forward(length)
-    anotherturt.left(90)
-    anotherturt.forward(length)
-    if depth != maxdepth:
-        turt3 = anotherturt.clone()
-        turt3.left(45)
-        turt3.forward(180*((sqrt(2)/2)**(1+depth)))
-        turt3.right(90)
-        fractal(turt3, depth+1, maxdepth)
-    anotherturt.left(90)
-    anotherturt.forward(length)
+from turtle import *
+tracer(0)
+delay(0)
+left(90)
+M=30
+screensize(8000,8000)
+for i in range(2):
+    fd(10*M)
+    right(90)
+    fd(18* M)
+    right(90)
+up()
+fd(5*M)
+rt(90)
+fd(7*M)
+left(90)
+down()
+for i in range(2):
+    fd(10*M)
+    right(90)
+    fd(7 * M)
+    right(90)
 
-def draw_fractal():
-    window = Screen()
-    t = Turtle()
-    t.hideturtle()
-    t.penup()
-    t.goto(-75, -225)
-    t.pendown()
-    t.speed(10)
-    t.left(90)
-    fractal(t, 1, 12)
-    window.exitonclick()
+up()
+for x in range(-50,100):
+    for y in range(-50,100):
+        goto(x*M,y*M)
+        dot(3,'green')
+exitonclick()
+update()
 
-draw_fractal()
+#26
+f=open('26-123.txt')
+#f=open('test.txt')
+M,N=map(int,f.readline().split())#M-кол парковок N-кол заявок
+kolsam=0
+parkovki=[[0,1]]*M
+b=[]
+for i in f:
+    vrem,prod,nomers,nomerk=map(int,i.split())
+    b.append([vrem,vrem+prod,nomers-1,nomerk-1])
+b.sort(key=lambda x:x[1])
+obshee = 0
+def sumpar(parkovki):
+    sums=0
+    for y in parkovki:
+        sums+=y[0]
+    return sums
+for i in range(1,2000):
+    for j in range(len(parkovki)):
+        parkovki[j][1]=1
+    for a in b:
+        k1,k2,l1,l2=a[0],a[1],a[2],a[3]
+        if i==k1:
+            if parkovki[l1][0]==0 and parkovki[l1][1]==1:
+                obshee+=1
+            elif parkovki[l1][0]>0:
+                parkovki[l1][0]-=1
+        elif i==k2:
+            parkovki[l2][0]+=1
+            parkovki[l2][1]=0
+    kolsam=max(obshee-sumpar(parkovki),kolsam)
+print(obshee,kolsam)
